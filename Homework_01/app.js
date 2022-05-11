11c269340ff9c387d18bfea8a2ce830f327d9a59
santos019 <santos019@naver.com>
2022-05-11 18:07:07 +0900

    css 수정해야된다

--- Homework_01/app.js
+++ Homework_01/app.js
@@ -1,12 +1,11 @@
 let output = localStorage.getItem('list')
-let loadingArr = JSON.parse(output)
+let loadingArr = JSON.parse(output) === null ? [] : JSON.parse(output)
+let countNumber = loadingArr.length === 0 ? 0 : loadingArr[loadingArr.length - 1].nodeId + 1
+
 const registerBtn = document.querySelector('.btnContext')
 const totalList = document.querySelector('.ContentsListContainer')
 const removeBtn = document.querySelector('.clearContext')
-
-if (loadingArr === null) loadingArr = []
-
-let countNumber = loadingArr.length === 0 ? 0 : loadingArr[loadingArr.length - 1].nodeId + 1
+const keyInput = document.querySelector('.btnInsert')
 
 if (localStorage.getItem('list') !== null) {
     for (const i in loadingArr) {
@@ -18,43 +17,44 @@ if (localStorage.getItem('list') !== null) {
             setChkBox.checked = true
             const changeDiv = newList.childNodes[2]
             const changeChkImg = newList.childNodes[1]
-            changeChkImg.classList.replace('FalseBox', 'TrueBox')
+            changeChkImg.classList.toggle('TrueBox')
             changeDiv.style.textDecoration = 'line-through'
         }
     }
-    console.log(loadingArr)
+    // console.log(loadingArr)
 }
 function paintList (input) {
     const removeBtnDiv = document.createElement('div')
     const newList = document.createElement('li')
     const liContextDiv = document.createElement('div')
-    const chkBox = document.createElement('input')
-    chkBox.id = 'check' + countNumber
-    chkBox.type = 'checkbox'
-    const chkLabel = document.createElement('label')
+    const checkDiv = document.createElement('input')
+    const checkLabel = document.createElement('label')
 
     liContextDiv.appendChild(input)
 
-    newList.appendChild(chkBox)
-    newList.appendChild(chkLabel)
+    newList.appendChild(checkDiv)
+    newList.appendChild(checkLabel)
     newList.appendChild(liContextDiv)
     newList.appendChild(removeBtnDiv)
-    newList.className = 'newList'
 
     totalList.appendChild(newList)
-    liContextDiv.className = 'liContextDiv' // + countNumber
+    checkDiv.id = 'check' + countNumber
+    checkDiv.type = 'checkbox'
+    newList.className = 'newList'
+    liContextDiv.className = 'liContextDiv'
     removeBtnDiv.className = 'removeBtnDiv'
-    chkBox.className = 'checkDiv'
-    chkLabel.className = 'checkLabel FalseBox'
-    chkLabel.for = 'check' + countNumber
-    // console.log(loadingArr)
+    checkDiv.className = 'checkDiv'
+    checkLabel.className = 'checkLabel'
+    checkLabel.for = 'check' + countNumber
     return newList
 }
 
 // 문제: 길이에 따라서 조정 필요함 순서가 보장된다는 전제하에 마지막 인덱스 값을 넣어준다.->순서보장안됨// 특정 인덱스에 최대 값을 저장한다. => value에 배열로 저장하면 된다.
 
 registerBtn.addEventListener('click', addList)
-function addList () {
+function addList (e) {
+    if ((e.type === 'keyup') && (e.key !== 'Enter')) { return }
+
     const InsertValue = document.querySelector('.btnInsert').value
     const Inserttext = document.createTextNode(InsertValue)
     const newList = paintList(Inserttext)
@@ -66,68 +66,46 @@ function addList () {
 }
 
 totalList.addEventListener('click', removeList)
-
+totalList.addEventListener('mouseover', hoverEvnt)
+totalList.addEventListener('mouseout', hoverEvnt)
+function hoverEvnt () {
+    if (event.target.classList.contains('checkLabel') || event.target.classList.contains('liContextDiv') || event.target.classList.contains('removeBtnDiv')) {
+        const evntArr = event.target.className.split(' ')
+        event.target.classList.toggle(evntArr[0] + 'hvEvnt')
+    }
+}
 function removeList () {
     output = localStorage.getItem('list')
     loadingArr = JSON.parse(output)
-    const deleteIndex = event.target.parentNode.id
-    if (event.target.className === 'removeBtnDiv') {
-        const upNode = event.target.parentNode
-        const moreUpNode = upNode.parentNode
-        console.log(deleteIndex)
+    const parNode = event.target.parentNode
+    const deleteIndex = parNode.id
+    if (event.target.classList.contains('removeBtnDiv')) {
+        const moreUpNode = parNode.parentNode
+        // console.log(deleteIndex)
         if (localStorage.getItem('list').length <= 1) {
             localStorage.clear()
             loadingArr = []
+            return
         } else {
-            moreUpNode.removeChild(upNode)
-            const filterArr = loadingArr.filter((el) => Number(el.nodeId) !== Number(deleteIndex))
-            console.log(filterArr)
-            localStorage.setItem('list', JSON.stringify(filterArr))
+            moreUpNode.removeChild(parNode)
+            loadingArr = loadingArr.filter((el) => Number(el.nodeId) !== Number(deleteIndex))
         }
-    } else if (event.target.className === 'liContextDiv') {
-        const labelDiv = event.target.previousSibling
-        if (event.target.style.textDecoration === 'line-through') {
-            event.target.style.textDecoration = 'none'
-            labelDiv.classList.replace('TrueBox', 'FalseBox')
-        } else {
-            event.target.style.textDecoration = 'line-through'
-            labelDiv.classList.replace('FalseBox', 'TrueBox')
-        }
+    } else if (event.target.classList.contains('liContextDiv') || event.target.classList.contains('checkLabel')) {
+        const checkDiv = parNode.childNodes[0]
+        const labelDiv = parNode.childNodes[1]
+        const listDiv = parNode.childNodes[2]
+        checkDiv.checked = !checkDiv.checked
+        labelDiv.classList.toggle('TrueBox')
+        listDiv.style.textDecoration = listDiv.style.textDecoration === 'line-through' ? 'none' : 'line-through'
+        // 이거 먼저 만들어진  find나 findindex 사용하는게 더 빠른지????
         // eslint-disable-next-line prefer-const
         for (let i in loadingArr) {
-            console.log(i)
             if (loadingArr[i].nodeId === Number(deleteIndex)) {
                 loadingArr[i].nodeCheck = !loadingArr[i].nodeCheck
-                console.log('in' + i)
             }
         }
-        console.log(loadingArr)
-        localStorage.setItem('list', JSON.stringify(loadingArr))
-    } else if (event.target.classList.contains('checkLabel')) {
-        const textDiv = event.target.nextSibling
-        const checkValue = event.target.previousSibling.checked
-
-        if (checkValue === false) {
-            // checkValue=true;
-            event.target.previousSibling.checked = true
-            event.target.classList.replace('FalseBox', 'TrueBox')
-            textDiv.style.textDecoration = 'line-through'
-        } else {
-            // checkValue=false;
-            event.target.previousSibling.checked = false
-            event.target.classList.replace('TrueBox', 'FalseBox')
-            textDiv.style.textDecoration = 'none'
-        }
-        for (let i in loadingArr) {
-            console.log(i)
-            if (loadingArr[i].nodeId === Number(deleteIndex)) {
-                loadingArr[i].nodeCheck = !loadingArr[i].nodeCheck
-                console.log('in' + i)
-            }
-        }
-        console.log(loadingArr)
-        localStorage.setItem('list', JSON.stringify(loadingArr))
     }
+    localStorage.setItem('list', JSON.stringify(loadingArr))
 }
 removeBtn.addEventListener('click', allRemoveList)
 function allRemoveList () {
@@ -140,21 +118,14 @@ function allRemoveList () {
     localStorage.clear()
 }
 
-removeBtn.addEventListener('mouseenter', removeBtnHover)
-function removeBtnHover () {
-    removeBtn.classList.replace('clearContext', 'clearContextHover')
+removeBtn.addEventListener('mouseenter', removeBtnChange)
+removeBtn.addEventListener('mouseleave', removeBtnChange)
+function removeBtnChange () {
+    removeBtn.classList.toggle('clearContextHover')
 }
-
-removeBtn.addEventListener('mouseout', removeBtnOver)
-function removeBtnOver () {
-    removeBtn.classList.replace('clearContextHover', 'clearContext')
+registerBtn.addEventListener('mouseenter', addBtnChange)
+registerBtn.addEventListener('mouseleave', addBtnChange)
+function addBtnChange () {
+    registerBtn.classList.toggle('btnContextChange')
 }
-
-registerBtn.addEventListener('mouseenter', addBtnHover)
-function addBtnHover () {
-    registerBtn.classList.replace('btnContext', 'btnContextHover')
-}
-registerBtn.addEventListener('mouseout', addBtnOver)
-function addBtnOver () {
-    registerBtn.classList.replace('btnContextHover', 'btnContext')
-}
+keyInput.addEventListener('keyup', e => addList(e))
